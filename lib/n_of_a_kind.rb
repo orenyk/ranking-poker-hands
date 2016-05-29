f98c303a1f5b021e10d905b18ba0f00d18bec715
Oren Kanner <okanner@gmail.com>
2016-05-29 18:33:08 -0400

    Add NOfAKind#valid?

--- lib/n_of_a_kind.rb
+++ lib/n_of_a_kind.rb
@@ -1,5 +1,17 @@
 require "card_group"
 
 class NOfAKind < CardGroup
+  def valid?
+    num_unique_values == 1
+  end
 
+  private
+
+  def card_values
+    cards.map { |card| card[:value] }
+  end
+
+  def num_unique_values
+    card_values.uniq.length
+  end
 end
--- spec/n_of_a_kind_spec.rb
+++ spec/n_of_a_kind_spec.rb
@@ -3,4 +3,22 @@ require "n_of_a_kind"
 
 describe NOfAKind do
   it_behaves_like "CardGroup"
+
+  describe "#valid?" do
+    it "returns true if all card values are the same" do
+      cards = [mock_card(2, "C"), mock_card(2, "H"), mock_card(2, "D")]
+
+      result = NOfAKind.new(cards: cards).valid?
+
+      expect(result).to be_truthy
+    end
+
+    it "returns false if all of the card values are not the same" do
+      cards = [mock_card(2, "C"), mock_card(2, "H"), mock_card(3, "D")]
+
+      result = NOfAKind.new(cards: cards).valid?
+
+      expect(result).to be_falsey
+    end
+  end
 end
